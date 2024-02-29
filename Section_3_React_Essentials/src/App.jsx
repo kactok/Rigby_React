af576b863cf8d0e7a496dbfdcef27073a8ed607b
kactok <104381556+kactok@users.noreply.github.com>
2024-02-29 21:21:23 +0100

    Section 3 - topics from 40 - 46 - props - dynamic values

--- Section_3_React_Essentials/src/App.jsx
+++ Section_3_React_Essentials/src/App.jsx
@@ -1,21 +1,50 @@
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS as cc } from "./data";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
 }
+
+function CoreConcepts({ image: i, title: t, description: d }) {
+  return (
+    <li>
+      <img src={i} alt={t} />
+      <h3>{t}</h3>
+      <p>{d}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <ul>
+            <CoreConcepts {...cc[0]} />
+            <CoreConcepts {...cc[1]} />
+            <CoreConcepts {...cc[2]} />
+            <CoreConcepts {...cc[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
