071509d60c87014681794b81c239147e8963d2d0
kactok <104381556+kactok@users.noreply.github.com>
2024-03-04 20:24:07 +0100

    Section 4 - Tic-tac-toe Game: - change username logic - wto-way-binding

---
+++ Section_4/src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          ></input>
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+        <button onClick={handleClick}>{isEditing ? "Save" : "Edit"}</button>
+      </span>
+    </li>
+  );
+}
