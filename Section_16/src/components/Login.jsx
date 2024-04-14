dd1dff04f4db9eab0cf8d600eb8fb3082f3d075c
kactok <104381556+kactok@users.noreply.github.com>
2024-04-14 21:20:16 +0200

    Section 16

---
+++ Section_16/src/components/Login.jsx
@@ -0,0 +1,35 @@
+import { useRef } from "react";
+
+export default function Login() {
+  const password = useRef();
+  const email = useRef();
+  function handleSubmit(event) {
+    event.preventDefault();
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+    console.log(enteredEmail, enteredPassword);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input ref={email} id="email" type="email" name="email" />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input ref={password} id="password" type="password" name="password" />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
